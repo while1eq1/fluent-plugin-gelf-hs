e8782421438e2e614abcd0e149d1f75d3caf68c8
Alex Yamauchi <alex.yamauchi@hotschedules.com>
2016-11-03 21:03:04 +0000

    INFRA-925: redoing plugins to use the functions now in GelfUtil.

--- lib/fluent/plugin/out_gelf.rb
+++ lib/fluent/plugin/out_gelf.rb
@@ -2,7 +2,10 @@ module Fluent
 
 class GELFOutput < BufferedOutput
 
-  Plugin.register_output("gelf", self)    
+  Plugin.register_output("gelf", self)
+
+  require_relative 'gelf_util'
+  include GelfUtil
 
   config_param :use_record_host, :bool, :default => false
   config_param :add_msec_time, :bool, :default => false
@@ -45,54 +48,15 @@ class GELFOutput < BufferedOutput
   end
 
   def format(tag, time, record)
-    gelfentry = { :timestamp => time, :_tag => tag }
 
-    record.each_pair do |k,v|
-      case k
-      when 'version' then
-        gelfentry[:_version] = v
-      when 'timestamp' then
-        gelfentry[:_timestamp] = v
-      when 'host' then
-        if @use_record_host then gelfentry[:host] = v
-        else gelfentry[:_host] = v end
-      when 'level' then
-        case "#{v}".downcase
-        # emergency and alert aren't supported by gelf-rb
-        when '0', 'emergency' then gelfentry[:level] = GELF::UNKNOWN
-        when '1', 'alert' then gelfentry[:level] = GELF::UNKNOWN
-        when '2', 'critical', 'crit' then gelfentry[:level] = GELF::FATAL
-        when '3', 'error', 'err' then gelfentry[:level] = GELF::ERROR
-        when '4', 'warning', 'warn' then gelfentry[:level] = GELF::WARN
-        # gelf-rb also skips notice
-        when '5', 'notice' then gelfentry[:level] = GELF::INFO
-        when '6', 'informational', 'info' then gelfentry[:level] = GELF::INFO
-        when '7', 'debug' then gelfentry[:level] = GELF::DEBUG
-        else gelfentry[:_level] = v
-        end
-      when 'msec' then
-        # msec must be three digits (leading/trailing zeroes)
-        if @add_msec_time then 
-          gelfentry[:timestamp] = "#{time.to_s}.#{v}".to_f
-        else
-          gelfentry[:_msec] = v
-        end
-      when 'short_message', 'full_message', 'facility', 'line', 'file' then
-        gelfentry[k] = v
-      else
-        gelfentry['_'+k] = v
-      end
-    end
+    make_gelfentry(
+      tag,time,record,
+      {
+        :use_record_host => @use_record_host,
+        :add_msec_time => @add_msec_time
+      }
+    ).to_msgpack
 
-    if !gelfentry.has_key?('short_message') then
-      if record.has_key?('message') then
-        gelfentry[:short_message] = record['message']
-      else
-        gelfentry[:short_message] = record.to_json
-      end
-    end
-
-    gelfentry.to_msgpack
   end
 
   def write(chunk)
